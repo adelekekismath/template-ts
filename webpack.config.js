da75c03a24c6ad8088747a7f4e9e5be76db6dd1f
kismath.adeleke <kismath.adeleke@habiteo.com>
2024-08-26 17:05:08 +0200

    update gitignore + webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,39 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'source-map',
-  target: 'node',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    target: 'web',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    },
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'GE Empty Template',
+            template: path.join(process.cwd(), 'src/index.html'),
+            filename: 'index.html',
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'GE Empty Template',
-    template: path.join(process.cwd(), 'src/index.html'),
-    filename: 'index.html',
-  })],
-  devServer: {
-    host: '0.0.0.0',
-    port: '1234',
-  }
+    devServer: {
+        host: '0.0.0.0',
+        port: '1234',
+    },
 };
